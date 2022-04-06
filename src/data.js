c60a69602fd9d1ef317b5e7bb44cf5022c6f14cd
Zuzanna Weichel <zu.weichel@gmail.com>
2022-04-06 03:30:16 -0400

    refactoring data

--- src/data.js
+++ src/data.js
@@ -1,31 +1,57 @@
 import avatar from './assets/avatar.svg';
+import uuid from 'react-uuid'
 
 
 export const posts = [
   {
+    id: uuid(),
+    timeStamp: new Date(),
+    text: "It's a-me, Mario!",
+    hypes: 2,
+    comments: [
+      {
+        id: uuid(),
+        timeStamp: new Date(),
+        text: "ha ha",
+        hypes: 0,
+        replies: 0,
+        shares: 0,
+        user: {
+          name: "Luigi",
+          avatar: avatar,
+        }
+      },
+      {
+        id: uuid(),
+        timeStamp: new Date(),
+        text: "Mario!!!",
+        hypes: 2,
+        replies: 3,
+        shares: 1,
+        user: {
+          name: "Luigi",
+          avatar: avatar,
+        }
+      }
+    ],
+    shares: 15,
+    views: 110,
     user: {
       name: "Mario",
       avatar: avatar
-    },
-    post: {
-      text: "It's a-me, Mario!",
-      hypes: 2,
-      comments: [{},{}],
-      shares: 15,
-      views: 110 
-    }
+    },   
   },
   {
+    id: uuid(),
+    timeStamp: new Date(),
+    text: "Hui hew! Just what I needed!",
+    hypes: 58,
+    comments: [],
+    shares: 63,
+    views: 101,
     user: {
       name: "Mario",
       avatar: avatar
-    },
-    post: {
-      text: "Hui hew! Just what I needed!",
-      hypes: 58,
-      comments: [],
-      shares: 63,
-      views: 101 
-    }
+    }, 
   }
 ]
