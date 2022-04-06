273f285e1966ec42aa038a5a7d69b9a37536193b
Zuzanna Weichel <zu.weichel@gmail.com>
2022-04-05 23:44:22 -0400

    Adding draft data

---
+++ src/data.js
@@ -0,0 +1,31 @@
+import logo from './assets/avatar.svg';
+
+
+export const posts = [
+  {
+    user: {
+      name: "Mario",
+      avatar: logo
+    },
+    post: {
+      text: "It's a-me, Mario!",
+      hypes: 2,
+      comments: [{},{}],
+      shares: 15,
+      views: 110 
+    }
+  },
+  {
+    user: {
+      name: "Mario",
+      avatar: logo
+    },
+    post: {
+      text: "Hui hew! Just what I needed!",
+      hypes: 58,
+      comments: [],
+      shares: 63,
+      views: 101 
+    }
+  }
+]
